de2db3ab0ecf8f9a7a8cab117d3aa39f4a45ab77
Kywi <54994309+Kywi@users.noreply.github.com>
2021-03-31 15:56:44 +0300

    Started Implements multiplayer. Added: Server brouser, host menu

--- Source/RocketGalaxy/StreamArcade.Build.cs
+++ Source/RocketGalaxy/StreamArcade.Build.cs
@@ -18,7 +18,7 @@ public class StreamArcade : ModuleRules
 
         // Uncomment if you are using online features
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
+        DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
         // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
     }
 }
